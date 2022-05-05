6b432c8d1cd76c92b621fc41995f0dc547769406
Nnamdi Okeke <onnamdialex@gmail.com>
2022-05-05 21:02:35 +0100

    :zap: search result fix

---
+++ src/context/ResultContextProvider.js
@@ -0,0 +1,39 @@
+import React, { createContext, useContext, useState } from "react";
+
+const ResultContext = createContext();
+const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
+
+export const ResultContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("JS Mastery");
+
+  const getResults = async (type) => {
+    setIsLoading(true);
+
+    const response = await fetch(`${baseUrl}${type}`, {
+      method: "GET",
+      headers: {
+        "X-User-Agent": "desktop",
+        "X-RapidAPI-Host": "google-search3.p.rapidapi.com",
+        "X-RapidAPI-Key": "baa38db7a9msh64dd74fda66bb72p14a910jsn16c29cf81576",
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    setResults(data);
+    setIsLoading(false);
+  };
+
+  return (
+    <ResultContext.Provider
+      value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}
+    >
+      {children}
+    </ResultContext.Provider>
+  );
+};
+
+export const useResultContext = () => useContext(ResultContext);
